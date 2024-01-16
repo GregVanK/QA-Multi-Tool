88287311300a0809fe38d8bfb0525d4f5f1ed137
GregV <gvankampen@upei.ca>
2024-01-16 11:17:14 -0400

    initialized project

---
+++ main.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+
+window = tk.Tk()
+window.configure()
+window.title("PLQA Multi-Tool by gvan")
+
+
+def handle_button_press():
+    window.destroy()
+
+
+button = tk.Button(text="die", command=handle_button_press)
+button.pack()
+
+window.mainloop()
+##apple
